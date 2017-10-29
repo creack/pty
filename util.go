ce2e34a61db1492e29ae490fa156d24a6312fc82
Dolf Schimmel <dolfschimmel@gmail.com>
2017-10-29 18:18:16 +0100

    Implement setSize()

--- util.go
+++ util.go
@@ -12,8 +12,19 @@ import (
 // in each line) in terminal t.
 func Getsize(t *os.File) (rows, cols int, err error) {
 	var ws winsize
-	err = windowrect(&ws, t.Fd())
+	err = windowrect(&ws, t.Fd(), false)
 	return int(ws.ws_row), int(ws.ws_col), err
+}
+
+func SetSize(t *os.File, rows, cols uint) error {
+	ws := &winsize{
+		ws_row:    uint16(rows),
+		ws_col:    uint16(cols),
+		ws_xpixel: 0,
+		ws_ypixel: 0,
+	}
+
+	return windowrect(ws, t.Fd(), true)
 }
 
 type winsize struct {
@@ -23,11 +34,18 @@ type winsize struct {
 	ws_ypixel uint16
 }
 
-func windowrect(ws *winsize, fd uintptr) error {
+func windowrect(ws *winsize, fd uintptr, set bool) error {
+	var call uintptr
+	if set {
+		call = syscall.TIOCSWINSZ
+	} else {
+		call = syscall.TIOCGWINSZ
+	}
+
 	_, _, errno := syscall.Syscall(
 		syscall.SYS_IOCTL,
 		fd,
-		syscall.TIOCGWINSZ,
+		call,
 		uintptr(unsafe.Pointer(ws)),
 	)
 	if errno != 0 {
