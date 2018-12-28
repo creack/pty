fad38bcf07a45d14e82c49690e5a18682fe9d2ac
Fazal Majid <fazal@majid.org>
2018-12-27 17:20:19 -0800

    fixes for Solaris ioctls, tested using github.com/yudai/gotty as a test bed

--- ioctl_solaris.go
+++ ioctl_solaris.go
@@ -6,9 +6,15 @@ import (
 )
 
 const (
+	// see /usr/include/sys/stropts.h
+	I_PUSH  = uintptr((int32('S')<<8 | 002))
 	I_STR   = uintptr((int32('S')<<8 | 010))
+	I_FIND  = uintptr((int32('S')<<8 | 013))
+	// see /usr/include/sys/ptms.h
 	ISPTM   = (int32('P') << 8) | 1
-	UNLKPT  = (int32('P') << 8) | 1
+	UNLKPT  = (int32('P') << 8) | 2
+	PTSSTTY = (int32('P') << 8) | 3
+	ZONEPT  = (int32('P') << 8) | 4
 	OWNERPT = (int32('P') << 8) | 5
 )
 
--- pty_solaris.go
+++ pty_solaris.go
@@ -16,11 +16,12 @@ import (
 const NODEV = ^uint64(0)
 
 func open() (pty, tty *os.File, err error) {
-	pFD, err := syscall.Open("/dev/ptmx", syscall.O_RDWR|syscall.O_CLOEXEC, 0)
+	masterfd, err := syscall.Open("/dev/ptmx", syscall.O_RDWR|unix.O_NOCTTY, 0)
+	//masterfd, err := syscall.Open("/dev/ptmx", syscall.O_RDWR|syscall.O_CLOEXEC|unix.O_NOCTTY, 0)
 	if err != nil {
 		return nil, nil, err
 	}
-	p := os.NewFile(uintptr(pFD), "/dev/ptmx")
+	p := os.NewFile(uintptr(masterfd), "/dev/ptmx")
 
 	sname, err := ptsname(p)
 	if err != nil {
@@ -37,10 +38,20 @@ func open() (pty, tty *os.File, err error) {
 		return nil, nil, err
 	}
 
-	t, err := os.OpenFile(sname, os.O_RDWR, 0)
+	slavefd, err := syscall.Open(sname, os.O_RDWR|unix.O_NOCTTY, 0)
 	if err != nil {
 		return nil, nil, err
 	}
+	t := os.NewFile(uintptr(slavefd), sname)
+
+	// pushing terminal driver STREAMS modules as per pts(7)
+	for _, mod := range([]string{"ptem", "ldterm", "ttcompat"}) {
+		err = streams_push(t, mod)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	
 	return p, t, nil
 }
 
@@ -67,7 +78,14 @@ func ptsname(f *os.File) (string, error) {
 	if dev == NODEV {
 		return "", errors.New("not a master pty")
 	}
-	return "/dev/pts/" + strconv.FormatInt(int64(dev), 10), nil
+	fn := "/dev/pts/" + strconv.FormatInt(int64(dev), 10)
+	// access(2) creates the slave device (if the pty exists)
+	// F_OK == 0 (unistd.h)
+	err := unix.Access(fn, 0)
+	if err != nil {
+		return "", err
+	}
+	return fn, nil
 }
 
 type pt_own struct {
@@ -98,4 +116,24 @@ func grantpt(f *os.File) error {
 func unlockpt(f *os.File) error {
 	istr := strioctl{UNLKPT, 0, 0, nil}
 	return ioctl(f.Fd(), I_STR, uintptr(unsafe.Pointer(&istr)))
+}
+
+// push STREAMS modules if not already done so
+func streams_push(f *os.File, mod string) error {
+	var err error
+	buf := []byte(mod)
+	// XXX I_FIND is not returning an error when the module
+	// is already pushed even though truss reports a return
+	// value of 1. A bug in the Go Solaris syscall interface?
+	// XXX without this we are at risk of the issue
+	// https://www.illumos.org/issues/9042
+	// but since we are not using libc or XPG4.2, we should not be
+	// double-pushing modules
+	
+	err = ioctl(f.Fd(), I_FIND, uintptr(unsafe.Pointer(&buf[0])))
+	if err != nil {
+		return nil
+	}
+	err = ioctl(f.Fd(), I_PUSH, uintptr(unsafe.Pointer(&buf[0])))
+	return err
 }
