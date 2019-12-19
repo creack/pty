e166309286e948ef687ef71b9d1f6ad0ed193297
Guillaume J. Charmes <gcharmes@magicleap.com>
2019-12-19 14:34:02 -0500

    Add test for noctty
    Signed-off-by: Guillaume J. Charmes <gcharmes@magicleap.com>

---
+++ ctty_test.go
@@ -0,0 +1,116 @@
+package pty_test
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"syscall"
+	"testing"
+
+	"github.com/creack/pty"
+)
+
+type testCmd struct {
+	*exec.Cmd
+	stdoutPipe io.Reader
+}
+
+func testDaemon(command string, args ...string) (*testCmd, error) {
+	// Create the command.
+	cmd := exec.Command(command, args...)
+
+	// Create a pipe to let the parent consume the output directly.
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("cmd.StdoutPipe: %w", err)
+	}
+	cmd.Stderr = cmd.Stdout
+	cmd.Env = os.Environ()
+
+	// Daemonize the process.
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+
+	// Start the process.
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("cmd.Start: %w", err)
+	}
+
+	return &testCmd{Cmd: cmd, stdoutPipe: stdoutPipe}, nil
+}
+
+func grepCmd() ([]byte, error) {
+	// Create a command with "grep".
+	c := exec.Command("grep", "bar")
+
+	// Start the command in a pty.
+	f, err := pty.Start(c)
+	defer func() { _ = f.Close() }() // Best effort.
+	if err != nil {
+		return nil, fmt.Errorf("pty.Start: %w", err)
+	}
+
+	// Send some data to grep and signal the end with EOT (ctrl-d).
+	go func() {
+		// Best effort.
+		_, _ = f.Write([]byte("foo\n"))
+		_, _ = f.Write([]byte("bar\n"))
+		_, _ = f.Write([]byte("baz\n"))
+		_, _ = f.Write([]byte{4}) // EOT.
+	}()
+
+	// Return the output of grep.
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		// Make sure we have the expected ptmx close error and not something else.
+		var pErr *os.PathError
+		if !errors.As(err, &pErr) || pErr.Op != "read" || pErr.Path != "/dev/ptmx" {
+			return nil, err
+		}
+	}
+	return buf, nil
+}
+
+func init() {
+	// If the binary is ran with that env variable, we are running the daemonized noctty test.
+	if os.Getenv("forked_TestDaemonizedPTY") == "test_fork_noctty" {
+		buf, err := grepCmd()
+		if err != nil {
+			log.Fatalf("grepCmd: %s", err)
+		}
+		fmt.Printf("%s", buf)
+		os.Exit(0)
+	}
+}
+
+// Global fork lock to make sure the test runs only once at a time.
+var testDaemonizePTYForkLock sync.Mutex
+
+func TestDaemonizedPTY(t *testing.T) {
+	// Lock the test to avoid issues with fork.
+	testDaemonizePTYForkLock.Lock()
+	defer testDaemonizePTYForkLock.Unlock()
+
+	// Set an env variable to tell the daemonized child to run the proper test.
+	_ = os.Setenv("forked_TestDaemonizedPTY", "test_fork_noctty")
+	defer os.Unsetenv("forked_TestDaemonizedPTY")
+
+	// Re-run the current binary in daemon mode (no ctty).
+	cmd, err := testDaemon(os.Args[0], os.Args[1:]...)
+	if err != nil {
+		t.Fatalf("Fork: %s.\n", err)
+	}
+	// Consume the output of the child process.
+	buf, err := ioutil.ReadAll(cmd.stdoutPipe)
+	if err != nil {
+		t.Fatalf("Read: %s.\n", err)
+	}
+	// Assert result.
+	if expect, got := "foo\r\nbar\r\nbaz\r\nbar\r\n", string(buf); expect != got {
+		t.Fatalf("Unexpected result.\nExpect:\t%q\nGot:\t%q\n", expect, got)
+	}
+}
