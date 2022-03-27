bf763050afc29a96514d2e17eb012de2ad9fc047
Guillaume J. Charmes <guillaume.charmes@immertec.com>
2022-03-27 13:40:59 -0400

    Fix compilation for AIX. Relates to #139

--- start_windows.go
+++ start_windows.go
@@ -16,5 +16,5 @@ import (
 // This will resize the pty to the specified size before starting the command.
 // Starts the process in a new session and sets the controlling terminal.
 func StartWithSize(cmd *exec.Cmd, ws *Winsize) (*os.File, error) {
-	return nil, errors.New("unsupported platform")
+	return nil, ErrUnsupported
 }
